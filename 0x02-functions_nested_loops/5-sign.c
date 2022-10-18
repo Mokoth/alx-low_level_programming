7eadaef586f7e554d014ff73a0dd3b6ca535b1e4
Mokoth <onyach.okoth1@outlook.com>
2022-10-18 14:23:22 -0700

    check sign after check

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - of a number
+ * Return: 1 and prints + if n is greater
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ *
+ * Return: 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+}
