3ed70aacc4c55edb17f5b7a2205b5a3107e21031
Mokoth <onyach.okoth1@outlook.com>
2022-10-18 13:24:49 -0700

    print 10xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * program to print 10xalphabets
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int n;
+
+	while (n < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar(10);
+		n++;
+	}
+}
