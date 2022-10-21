4f8cff3ae6e27e800e0fb4689448ac414b0b5f67
Mokoth <onyach.okoth1@outlook.com>
2022-10-21 11:33:40 -0700

    check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isuper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isuper - checks for uppercase character
+ * @c: the charatcter to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isuper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
