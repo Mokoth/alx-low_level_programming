2299e0cec8b8b624fda4cf93f9cdc200433af4b1
Mokoth <onyach.okoth1@outlook.com>
2022-10-21 18:17:32 -0700

    check for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the charatcter to be checked
+ * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _issupe(int c)
+int _issuper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
 }
