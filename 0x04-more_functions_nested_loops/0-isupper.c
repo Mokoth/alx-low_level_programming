934b29120e8300855236732d42d2be15e3ac234d
Mokoth <onyach.okoth1@outlook.com>
2022-10-21 17:58:42 -0700

    check for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isuper - checks for uppercase character
+ * _isupper - checks for uppercase character
  * @c: the charatcter to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
